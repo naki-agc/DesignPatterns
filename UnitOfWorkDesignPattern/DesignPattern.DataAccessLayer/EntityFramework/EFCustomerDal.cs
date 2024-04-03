e9ad6e31a02458373902527485d06ec5a3b96983
Naki <nakiagca10@gmail.com>
2024-04-03 17:14:03 +0300

    Unit Of Work Design Pattern

---
+++ UnitOfWorkDesignPattern/DesignPattern.DataAccessLayer/EntityFramework/EFCustomerDal.cs
@@ -0,0 +1,20 @@
+﻿using DesignPattern.DataAccessLayer.Abstract;
+using DesignPattern.DataAccessLayer.Concrete;
+using DesignPattern.DataAccessLayer.Repositories;
+using DesignPattern.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.DataAccessLayer.EntityFramework
+{
+    public class EFCustomerDal:GenericRepository<Customer>,ICustomerDal
+    {
+        public EFCustomerDal(Context context):base(context)
+        {
+                
+        }
+    }
+}
